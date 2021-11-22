25698f867f54dcbf2b244ad698bfe5d66de28598
phneutral26 <philerichartmann@gmail.com>
2021-11-22 08:11:30 +0100

    timmoc

---
+++ dumbshit.py
@@ -0,0 +1,10 @@
+import random as r
+print("You are: ")
+print(a:=r.randint(12, 77))
+f = open('GG\gg.txt', 'r')
+def main():
+    print("Read the German law from the terminal!")
+    file_contents = f.read()
+    print(file_contents)
+
+main()

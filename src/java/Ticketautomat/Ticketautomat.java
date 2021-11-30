94863e3a7de52ac4ffbf9d0e8bb54eed789c8c47
phneutral26 <philerichartmann@gmail.com>
2021-11-30 10:21:24 +0100

    IDK what i did

--- src/java/Ticketautomat/Ticketautomat.java
+++ src/java/Ticketautomat/Ticketautomat.java
@@ -2,8 +2,8 @@
 /**
  * Write a description of class Ticketautomat here.
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Phil)
+ * @version (12/2021)
  */
 public class Ticketautomat
 {
